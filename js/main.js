55699e5aa55f1220b8357c8341a317ace640be37
ChangGyu-Ryu <ckue21@gmail.com>
2024-07-22 13:37:43 +0900

    #헤더와 드롭다운 메뉴 - 전역배치(GSAP) 처리

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ searchInputEl.addEventListener('blur', function() {
   searchInputEl.setAttribute('placeholder', '');
   searchInputEl.value = "";
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배치 숨기기 
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+
+  } else {
+    // 배치 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+
+}, 300));
+// _.throttle(함수, 시간)
